16f2835dbb397f1110b69a93b7b5b9d3f10eb005
Jody Frankowski <jody.frankowski@gmail.com>
2016-09-13 20:35:40 +0200

    Add solutions to chapter 01.

---
+++ chapter-01.el
@@ -0,0 +1,15 @@
+;; • Generate an error message by evaluating an appropriate symbol that
+;; is not within parentheses.
++
+
+;; • Generate an error message by evaluating an appropriate symbol that
+;; is between parentheses.
+(0)
+
+;; • Create a counter that increments by two rather than one.
+(setq counter 0)
+(setq counter (+ counter 2))
+
+;; • Write an expression that prints a message in the echo area when
+;; evaluated.
+(message "A message.")
