d9e302bda345b0d9d6d0c95e413de4d140f8fdb3
Jody Frankowski <jody.frankowski@gmail.com>
2016-09-14 11:25:40 +0200

    Improve a solution of chapter 01 exercises

--- chapter-01.el
+++ chapter-01.el
@@ -9,6 +9,7 @@
 ;; • Create a counter that increments by two rather than one.
 (setq counter 0)
 (setq counter (+ counter 2))
+counter
 
 ;; • Write an expression that prints a message in the echo area when
 ;; evaluated.
